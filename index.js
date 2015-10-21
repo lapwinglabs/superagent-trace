a2abccd79c12d20f1024ba1607cd6423958978ce
Matthew Mueller <mattmuelle@gmail.com>
2015-10-21 14:58:17 -0700

    x-response-time

--- index.js
+++ index.js
@@ -15,24 +15,43 @@ module.exports = trace
  * Initialize the `trace`
  *
  * @param {Superagent} superagent
+ * @param {Object} options
  * @return {Superagent}
  */
 
-function trace (superagent, log) {
-  log = log || debug('superagent:trace')
+function trace (superagent, options) {
+  options = options || {}
+
+  // options.logging
+  var log = options.log || debug('superagent:trace')
   log = typeof debug === 'string' ? debug(log) : log
-  var end = superagent.Request.prototype.end
+
+  // options.verbose
+  var verbose = options.verbose === undefined ? false : options.verbose
+
+  // options.slow
+  var slow = options.slow || 1000
 
   // Monkey-path end
+  var end = superagent.Request.prototype.end
   superagent.Request.prototype.end = function (fn) {
     var url = serialize(this)
     var start = new Date()
 
-    log('--> %s', url)
+    verbose && log('--> %s', url)
     end.call(this, function (err, res) {
-      if (err) return fn(err, res)
-      log('<-- %s (%s)', url, res.status)
-      return fn(err, res)
+      var time = res.headers['x-response-time']
+      if (err) {
+        time
+          ? log(' x  %s (%s %s)', url, res.status, time)
+          : log(' x  %s (%s)', url, res.status)
+        return fn(err, res)
+      } else {
+        time
+          ? log('<-- %s (%s %s)', url, res.status, time)
+          : log('<-- %s (%s)', url, res.status)
+        return fn(err, res)
+      }
     })
   }
 
